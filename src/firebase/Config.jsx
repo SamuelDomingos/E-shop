42bca053b204d7c0cae24b69599847f7bed5f33d
SamuelDomingos <89481523+SamuelDomingos@users.noreply.github.com>
2023-02-22 22:18:58 -0300

    Update Config.jsx

--- src/firebase/Config.jsx
+++ src/firebase/Config.jsx
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD33k3Sov_BDfzwbKzp_kQ-kkaDy5-eM-A",
-  authDomain: "ecommerce-fff48.firebaseapp.com",
-  projectId: "ecommerce-fff48",
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
   storageBucket: "ecommerce-fff48.appspot.com",
   messagingSenderId: "68717653896",
-  appId: "1:68717653896:web:af7eec25165d2a2716143d",
+  appId: "appId",
   measurementId: "G-6EM3V4JZ5L"
 };
 
